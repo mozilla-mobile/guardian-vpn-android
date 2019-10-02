7fa086667a479b3c8bad0d4911d8e53ac35e3e4a
Kami <yichen@mozilla.com>
2019-10-02 12:48:30 +0800

    Modify entrance UI

--- app/src/main/java/org/mozilla/guardian/splash/SplashActivity.kt
+++ app/src/main/java/org/mozilla/guardian/splash/SplashActivity.kt
@@ -16,5 +16,6 @@ class SplashActivity : AppCompatActivity() {
         splash_root.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
 
         startActivity(OnboardingActivity.getStartIntent(this))
+        finish()
     }
 }
