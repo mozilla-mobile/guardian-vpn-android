f95f9ea6455f9e7f2e72fe54e10accfdfc608b1e
maliu <max@mxli.us>
2019-10-03 11:45:07 +0800

    Add Mock up UI for Settings page

--- app/src/main/java/org/mozilla/guardian/main/settings/SettingsFragment.kt
+++ app/src/main/java/org/mozilla/guardian/main/settings/SettingsFragment.kt
@@ -17,10 +17,6 @@ class SettingsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         settingsViewModel = ViewModelProviders.of(this).get(SettingsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_settings, container, false)
-        val textView: TextView = root.findViewById(R.id.title)
-        settingsViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
         return root
     }
 }
