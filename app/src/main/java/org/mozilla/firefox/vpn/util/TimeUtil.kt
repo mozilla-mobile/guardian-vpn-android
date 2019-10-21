0342ce8028746b72ff3a71a058b6c329118957f8
Roger <pharspher@gmail.com>
2019-10-21 15:59:02 +0800

    Add a time utility class

---
+++ app/src/main/java/org/mozilla/firefox/vpn/util/TimeUtil.kt
@@ -0,0 +1,35 @@
+package org.mozilla.firefox.vpn.util
+
+import android.util.TimeFormatException
+import com.google.gson.internal.bind.util.ISO8601Utils
+import java.text.ParsePosition
+import java.util.*
+
+object TimeUtil {
+    private val zoneUtc = TimeZone.getTimeZone("UTC")
+
+    fun now(): Date {
+        return Calendar.getInstance(zoneUtc).time
+    }
+
+    @Throws(TimeFormatException::class)
+    fun parse(time: String, format: TimeFormat): Date {
+        try {
+            return format.parse(time)
+        } catch (e: Exception) {
+            throw TimeFormatException(e.message.orEmpty(), e)
+        }
+    }
+}
+
+interface TimeFormat {
+    fun parse(time: String): Date
+
+    object Iso8601 : TimeFormat {
+        override fun parse(time: String): Date {
+            return ISO8601Utils.parse(time, ParsePosition(0))
+        }
+    }
+}
+
+class TimeFormatException(msg: String, cause: Throwable): RuntimeException(msg, cause)
