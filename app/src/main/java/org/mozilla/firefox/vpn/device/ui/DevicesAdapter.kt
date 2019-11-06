db67d8ae3891f8db3d6664c74f3c01a8d9a04266
Roger <pharspher@gmail.com>
2019-11-06 15:45:29 +0800

    Show "Current device" under the current device item

--- app/src/main/java/org/mozilla/firefox/vpn/device/ui/DevicesAdapter.kt
+++ app/src/main/java/org/mozilla/firefox/vpn/device/ui/DevicesAdapter.kt
@@ -4,6 +4,7 @@ import android.text.format.DateUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.item_device.*
@@ -12,10 +13,9 @@ import org.mozilla.firefox.vpn.R
 import org.mozilla.firefox.vpn.device.data.CurrentDevice
 import org.mozilla.firefox.vpn.device.ui.DevicesAdapter.DevicesViewHolder
 import org.mozilla.firefox.vpn.service.DeviceInfo
+import org.mozilla.firefox.vpn.util.TimeFormat
+import org.mozilla.firefox.vpn.util.TimeUtil
 import java.text.ParseException
-import java.text.SimpleDateFormat
-import java.util.Locale
-import java.util.TimeZone
 
 class DevicesAdapter(
     private val devicesModel: DevicesModel,
@@ -23,7 +23,10 @@ class DevicesAdapter(
 ) : RecyclerView.Adapter<DevicesViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DevicesViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_device, parent, false)
+        val itemView = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.item_device, parent, false)
+
         val holder = DevicesViewHolder(itemView)
 
         holder.itemView.delete.setOnClickListener {
@@ -41,30 +44,33 @@ class DevicesAdapter(
         holder.bind(devicesModel.devices[position], devicesModel.currentDevice)
     }
 
-    class DevicesViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer {
+    class DevicesViewHolder(
+        override val containerView: View
+    ) : RecyclerView.ViewHolder(containerView), LayoutContainer {
 
         fun bind(device: DeviceInfo, currentDevice: CurrentDevice?) {
             title.text = device.name
             time.text = getRelativeTime(device.createdAt)
 
             val isCurrentDevice = currentDevice?.device == device
-            delete.visibility = if (isCurrentDevice) {
-                View.INVISIBLE
+            if (isCurrentDevice) {
+                time.text = itemView.context.getString(R.string.devices_current_device)
+                time.setTextColor(ContextCompat.getColor(itemView.context, R.color.science_blue))
+                delete.visibility = View.INVISIBLE
             } else {
-                View.VISIBLE
+                time.text = getRelativeTime(device.createdAt)
+                time.setTextColor(ContextCompat.getColor(itemView.context, R.color.abbey_alpha_60))
+                delete.visibility = View.VISIBLE
             }
         }
 
         private fun getRelativeTime(iso8601Time: String): String? {
-            val format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.US).apply {
-                timeZone = TimeZone.getTimeZone("UTC")
-            }
-
             val time = try {
-                format.parse(iso8601Time)
+                TimeUtil.parse(iso8601Time, TimeFormat.Iso8601)
             } catch (e: ParseException) {
                 return null
             }.time
+
 
             val now = System.currentTimeMillis()
             return DateUtils.getRelativeTimeSpanString(time, now, DateUtils.MINUTE_IN_MILLIS).toString()
