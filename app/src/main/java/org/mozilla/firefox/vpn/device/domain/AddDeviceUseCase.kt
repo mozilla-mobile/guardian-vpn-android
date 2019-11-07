31b95fa9c6800f94fce935cde4526b0ba28237de
Roger <pharspher@gmail.com>
2019-11-07 17:25:40 +0800

    Append a serial number to device name if the current device name is existed in the user's device list

--- app/src/main/java/org/mozilla/firefox/vpn/device/domain/AddDeviceUseCase.kt
+++ app/src/main/java/org/mozilla/firefox/vpn/device/domain/AddDeviceUseCase.kt
@@ -2,8 +2,10 @@ package org.mozilla.firefox.vpn.device.domain
 
 import org.mozilla.firefox.vpn.device.data.DeviceRepository
 import org.mozilla.firefox.vpn.service.DeviceInfo
+import org.mozilla.firefox.vpn.service.UnauthorizedException
 import org.mozilla.firefox.vpn.user.data.UserRepository
 import org.mozilla.firefox.vpn.util.Result
+import kotlin.math.max
 
 class AddDeviceUseCase(
     private val deviceRepository: DeviceRepository,
@@ -11,8 +13,37 @@ class AddDeviceUseCase(
 ) {
 
     suspend operator fun invoke(token: String): Result<DeviceInfo> {
-        return deviceRepository.addDevice(android.os.Build.MODEL, "Bearer $token").apply {
+        val userInfo = userRepository.getUserInfo() ?: return Result.Fail(UnauthorizedException())
+        val devices = userInfo.user.devices
+        val deviceName = findAvailableDeviceName(android.os.Build.MODEL, devices.map { it.name })
+
+        return deviceRepository.addDevice(deviceName, "Bearer $token").apply {
             userRepository.refreshUserInfo()
+        }
+    }
+
+    companion object {
+        fun findAvailableDeviceName(deviceName: String, existNames: List<String>): String {
+            var idx = 1
+            var maxIdx = idx
+
+            val regex = """$deviceName( \((\d+)\))?""".toRegex()
+
+            existNames.forEach { existName ->
+                regex.find(existName)?.let { result ->
+                    idx++
+
+                    result.groupValues[2].toIntOrNull()?.let { i ->
+                        maxIdx = max(maxIdx, i)
+                    }
+                }
+            }
+
+            return if (idx == 1) {
+                deviceName
+            } else {
+                "$deviceName (${maxIdx + 1})"
+            }
         }
     }
 }
