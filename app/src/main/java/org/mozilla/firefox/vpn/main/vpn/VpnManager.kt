9384d395adf29b29aa308a1dc311c62e3527f792
Roger <pharspher@gmail.com>
2019-11-12 14:51:03 +0800

    Add dependencies to VpnViewModel (#67)
    * Add dependencies to VpnViewModel
    
    * Remove test code

---
+++ app/src/main/java/org/mozilla/firefox/vpn/main/vpn/VpnManager.kt
@@ -0,0 +1,53 @@
+package org.mozilla.firefox.vpn.main.vpn
+
+import android.content.ComponentName
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.AsyncTask
+import android.os.Handler
+import android.os.Looper
+import com.wireguard.android.backend.GoBackend
+import com.wireguard.android.model.Tunnel
+import com.wireguard.android.model.TunnelManager
+import com.wireguard.android.util.AsyncWorker
+import com.wireguard.config.Config
+import org.mozilla.firefox.vpn.backend.FileConfigStore
+import org.mozilla.firefox.vpn.main.MainActivity
+
+class VpnManager(
+    private val appContext: Context,
+    private val prefs: SharedPreferences
+) {
+
+    private val tunnelManager = TunnelManager(
+        appContext,
+        prefs,
+        GoBackend(
+            appContext,
+            // TODO: Fix this weird dependency
+            ComponentName(appContext, MainActivity::class.java)
+        ),
+        FileConfigStore(),
+        AsyncWorker(AsyncTask.SERIAL_EXECUTOR, Handler(Looper.getMainLooper()))
+    )
+
+    private var tunnel: Tunnel? = null
+
+    fun isGranted(): Boolean {
+        return GoBackend.VpnService.prepare(appContext) != null
+    }
+
+    fun connect(name: String, config: Config) {
+        tunnel = tunnelManager.create(config, name)
+        tunnel?.state = Tunnel.State.UP
+    }
+
+    fun disconnect() {
+        tunnel?.state = Tunnel.State.DOWN
+        tunnel = null
+    }
+
+    fun isConnected(): Boolean {
+        return tunnel?.state == Tunnel.State.UP
+    }
+}
--- app/src/main/java/org/mozilla/firefox/vpn/main/vpn/VpnViewModel.kt
+++ app/src/main/java/org/mozilla/firefox/vpn/main/vpn/VpnViewModel.kt
@@ -3,8 +3,16 @@ package org.mozilla.firefox.vpn.main.vpn
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import org.mozilla.firefox.vpn.device.data.DeviceRepository
+import org.mozilla.firefox.vpn.servers.data.ServerRepository
+import org.mozilla.firefox.vpn.user.data.UserRepository
 
-class VpnViewModel : ViewModel() {
+class VpnViewModel(
+    private val vpnManager: VpnManager,
+    private val userRepository: UserRepository,
+    private val deviceRepository: DeviceRepository,
+    private val serverRepository: ServerRepository
+) : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "Click to toggle VPN on/off"
