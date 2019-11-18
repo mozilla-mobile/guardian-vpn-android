74e257f67da780910de3df9f83081b7624ae59ef
Kami <yichen@mozilla.com>
2019-11-18 17:52:14 +0800

    Modify typeface usage

--- app/src/main/java/org/mozilla/firefox/vpn/util/EmojiUtil.kt
+++ app/src/main/java/org/mozilla/firefox/vpn/util/EmojiUtil.kt
@@ -1,54 +1,27 @@
 package org.mozilla.firefox.vpn.util
 
 import android.content.Context
-import android.graphics.Typeface
-import android.widget.TextView
 import androidx.emoji.bundled.BundledEmojiCompatConfig
 import androidx.emoji.text.EmojiCompat
 import java.lang.Exception
-import java.util.*
+import java.util.Locale
 
 object EmojiUtil {
-    private var isInit = false
-    private var mTypeface: Typeface? = null
 
     fun initEmoji(context: Context) {
         val config = BundledEmojiCompatConfig(context)
         EmojiCompat.init(config)
-
-        try {
-            mTypeface = Typeface.createFromAsset(context.assets, "TwemojiMozilla.ttf")
-        } catch (e: Exception) {
-        }
-        isInit = true
     }
 
-    fun process(emoji: String): CharSequence {
-        if (!isInit) {
-            return ""
-        }
+    fun loadEmoji(codePoint: String): CharSequence {
         return try {
-            EmojiCompat.get().process(emoji)
+            EmojiCompat.get().process(codePoint)
         } catch (e: Exception) {
             ""
         }
     }
 
-    fun loadEmoji(textView: TextView, emoji: String) {
-        if (!isInit) {
-            return
-        }
-        try {
-            val emojiChar = process(emoji)
-            if (mTypeface != null) {
-                textView.typeface = mTypeface
-            }
-            textView.text = emojiChar
-        } catch (e: Exception) {
-        }
-    }
-
-    fun localeEmoji(countryCode: String): String {
+    fun getCountryFlagCodePoint(countryCode: String): String {
         val countryLetter = countryCode.toUpperCase(Locale.getDefault())
         val firstLetter = Character.codePointAt(countryLetter, 0) - 0x41 + 0x1F1E6
         val secondLetter = Character.codePointAt(countryLetter, 1) - 0x41 + 0x1F1E6
