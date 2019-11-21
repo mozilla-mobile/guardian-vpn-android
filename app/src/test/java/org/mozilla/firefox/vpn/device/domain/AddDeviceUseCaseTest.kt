365c0acea6468ffdfdeca12a41d9bfa00927d126
Roger <pharspher@gmail.com>
2019-11-21 16:52:40 +0800

    Fix test build break

--- app/src/test/java/org/mozilla/firefox/vpn/device/domain/AddDeviceUseCaseTest.kt
+++ app/src/test/java/org/mozilla/firefox/vpn/device/domain/AddDeviceUseCaseTest.kt
@@ -2,6 +2,7 @@ package org.mozilla.firefox.vpn.device.domain
 
 import org.junit.Assert.*
 import org.junit.Test
+import org.mozilla.firefox.vpn.util.findAvailableDeviceName
 
 class AddDeviceUseCaseTest {
 
@@ -11,30 +12,30 @@ class AddDeviceUseCaseTest {
 
         /** no device */
         var exists = listOf<String>()
-        assertEquals(deviceName, AddDeviceUseCase.findAvailableDeviceName(deviceName, exists))
+        assertEquals(deviceName, findAvailableDeviceName(deviceName, exists))
 
         /** {"model"} returns "model (2)" */
         exists = listOf(deviceName)
-        assertEquals("$deviceName (2)", AddDeviceUseCase.findAvailableDeviceName(deviceName, exists))
+        assertEquals("$deviceName (2)", findAvailableDeviceName(deviceName, exists))
 
         /** {"model", "model (2)"} returns "model (3)" */
         exists = listOf(deviceName, "$deviceName (2)")
-        assertEquals("$deviceName (3)", AddDeviceUseCase.findAvailableDeviceName(deviceName, exists))
+        assertEquals("$deviceName (3)", findAvailableDeviceName(deviceName, exists))
 
         /** {"model", "model (3)"} returns "model (4)" */
         exists = listOf(deviceName, "$deviceName (3)")
-        assertEquals("$deviceName (4)", AddDeviceUseCase.findAvailableDeviceName(deviceName, exists))
+        assertEquals("$deviceName (4)", findAvailableDeviceName(deviceName, exists))
 
         /** {"model", "dog", "model (3)"} returns "model (4)" */
         exists = listOf(deviceName, "dog", "$deviceName (3)")
-        assertEquals("$deviceName (4)", AddDeviceUseCase.findAvailableDeviceName(deviceName, exists))
+        assertEquals("$deviceName (4)", findAvailableDeviceName(deviceName, exists))
 
         /** {"model", "dog", "model (1)"} returns "model (2)" */
         exists = listOf(deviceName, "dog", "$deviceName (1)")
-        assertEquals("$deviceName (2)", AddDeviceUseCase.findAvailableDeviceName(deviceName, exists))
+        assertEquals("$deviceName (2)", findAvailableDeviceName(deviceName, exists))
 
         /** {"dog", "model (2)"} returns "model (3)" */
         exists = listOf("$deviceName (2)")
-        assertEquals("$deviceName (3)", AddDeviceUseCase.findAvailableDeviceName(deviceName, exists))
+        assertEquals("$deviceName (3)", findAvailableDeviceName(deviceName, exists))
     }
 }
