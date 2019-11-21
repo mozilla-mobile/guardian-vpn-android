6c6dea3c89ba43293ddf58157ef1e9dcb58152e9
Roger <pharspher@gmail.com>
2019-11-21 15:13:25 +0800

    Implement device-limit-reached flow

--- app/src/main/java/org/mozilla/firefox/vpn/device/ui/DevicesViewModel.kt
+++ app/src/main/java/org/mozilla/firefox/vpn/device/ui/DevicesViewModel.kt
@@ -5,26 +5,33 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.mozilla.firefox.vpn.UserStates
 import org.mozilla.firefox.vpn.device.data.CurrentDevice
+import org.mozilla.firefox.vpn.device.domain.AddDeviceUseCase
 import org.mozilla.firefox.vpn.device.domain.CurrentDeviceUseCase
 import org.mozilla.firefox.vpn.device.domain.GetDevicesUseCase
 import org.mozilla.firefox.vpn.device.domain.RemoveDeviceUseCase
+import org.mozilla.firefox.vpn.isDeviceLimitReached
 import org.mozilla.firefox.vpn.service.DeviceInfo
 import org.mozilla.firefox.vpn.service.UnauthorizedException
+import org.mozilla.firefox.vpn.shouldRegisterDevice
 import org.mozilla.firefox.vpn.user.domain.GetUserInfoUseCase
 import org.mozilla.firefox.vpn.util.Result
 import org.mozilla.firefox.vpn.util.TimeFormat
-import org.mozilla.firefox.vpn.util.TimeFormatException
 import org.mozilla.firefox.vpn.util.TimeUtil
+import org.mozilla.firefox.vpn.util.findAvailableModelName
 
 class DevicesViewModel(
     private val getDevicesUseCase: GetDevicesUseCase,
     private val removeDevicesUseCase: RemoveDeviceUseCase,
     private val currentDeviceUseCase: CurrentDeviceUseCase,
-    private val getUserInfoUseCase: GetUserInfoUseCase
+    private val getUserInfoUseCase: GetUserInfoUseCase,
+    private val addDeviceUseCase: AddDeviceUseCase,
+    private val userStates: UserStates
 ) : ViewModel() {
 
-    val devices = MutableLiveData<DevicesModel>()
+    val devices = MutableLiveData<DevicesUiModel>()
     val deviceCount = MutableLiveData<Pair<Int, Int>>()
     val isAuthorized = MutableLiveData<Boolean>(true)
 
@@ -36,20 +43,26 @@ class DevicesViewModel(
 
     fun deleteDevice(device: DeviceInfo) = viewModelScope.launch(Dispatchers.IO) {
         when (val result = removeDevicesUseCase(device.pubKey)) {
-            is Result.Success -> refreshDevices()
+            is Result.Success -> onDeviceDeleted()
             is Result.Fail -> handleFail(result.exception)
         }
     }
 
-    private fun refreshDevices() = viewModelScope.launch(Dispatchers.Main) {
-        val current = currentDeviceUseCase()
+    private suspend fun onDeviceDeleted() {
+        if (userStates.state.shouldRegisterDevice()) {
+            registerNewDevice()
+        }
+        refreshDevices()
+    }
+
+    private suspend fun registerNewDevice() {
+        val token = getUserInfoUseCase()?.token ?: return
+        addDeviceUseCase(token)
+    }
+
+    private suspend fun refreshDevices() = withContext(Dispatchers.Main) {
         when (val result = getDevicesUseCase()) {
-            is Result.Success -> {
-                val list = result.value.sortedByDescending {
-                    TimeUtil.parseOrNull(it.createdAt, TimeFormat.Iso8601)?.time ?: Long.MIN_VALUE
-                }
-                devices.value = DevicesModel(list, current)
-            }
+            is Result.Success -> notifyDevicesChanged(result.value)
             is Result.Fail -> handleFail(result.exception)
         }
 
@@ -58,17 +71,43 @@ class DevicesViewModel(
         }
     }
 
+    private suspend fun notifyDevicesChanged(list: List<DeviceInfo>) {
+        val sorted = list.sortedByDescending {
+            TimeUtil.parseOrNull(it.createdAt, TimeFormat.Iso8601)?.time ?: Long.MIN_VALUE
+        }.toMutableList()
+
+        val limitReached = userStates.state.isDeviceLimitReached()
+
+        val current = if (limitReached) {
+            val info = createDummyDevice(list)
+            sorted.add(0, info)
+            CurrentDevice(info, "")
+        } else {
+            currentDeviceUseCase()
+        }
+
+        devices.value = DevicesUiModel(sorted, current, limitReached)
+    }
+
+    private fun createDummyDevice(existDevices: List<DeviceInfo>): DeviceInfo {
+        val name = findAvailableModelName(existDevices)
+        return DeviceInfo(name, "", "", "", "")
+    }
+
     private fun handleFail(exception: Exception) {
         when (exception) {
-            is UnauthorizedException -> {
-                isAuthorized.value = false
-                devices.value = DevicesModel(emptyList(), null)
-            }
+            is UnauthorizedException -> handleUnauthorizedException()
         }
+    }
+
+    private fun handleUnauthorizedException() {
+        isAuthorized.value = false
+        devices.value = DevicesUiModel(emptyList(), null, false)
     }
 }
 
-data class DevicesModel(
+data class DevicesUiModel(
     val devices: List<DeviceInfo>,
-    val currentDevice: CurrentDevice?
+    val currentDevice: CurrentDevice?,
+    val isLimitReached: Boolean
 )
