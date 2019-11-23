752897cc9a129d6a3202d93d828da3f78bebd84a
Roger <pharspher@gmail.com>
2019-11-24 02:09:04 +0800

    Splash will now recognize the user is logged out and use different intent to launch onboarding activity

--- app/src/main/java/org/mozilla/firefox/vpn/splash/SplashViewModel.kt
+++ app/src/main/java/org/mozilla/firefox/vpn/splash/SplashViewModel.kt
@@ -7,22 +7,35 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.mozilla.firefox.vpn.UserState
 import org.mozilla.firefox.vpn.UserStates
-import org.mozilla.firefox.vpn.user.data.UserRepository
+import org.mozilla.firefox.vpn.user.data.checkAuth
+import org.mozilla.firefox.vpn.user.domain.LogoutUseCase
+import org.mozilla.firefox.vpn.user.domain.RefreshUserInfoUseCase
 
 class SplashViewModel(
-    private val userRepository: UserRepository,
+    refreshUserInfoUseCase: RefreshUserInfoUseCase,
+    logoutUseCase: LogoutUseCase,
     userStates: UserStates
 ) : ViewModel() {
 
     val showOnboarding = MutableLiveData<Unit>()
+    val showLoggedOutOnboarding = MutableLiveData<Unit>()
     val showMainPage = MutableLiveData<Unit>()
 
     init {
         viewModelScope.launch(Dispatchers.IO) {
-            userRepository.refreshUserInfo()
+            val oldState = userStates.state
+            refreshUserInfoUseCase().checkAuth(
+                unauthorized = {
+                    logoutUseCase()
+                }
+            )
 
             when (userStates.state) {
-                UserState.Login -> showOnboarding.postValue(Unit)
+                UserState.Login -> if (oldState != UserState.Login) {
+                    showLoggedOutOnboarding.postValue(Unit)
+                } else {
+                    showOnboarding.postValue(Unit)
+                }
                 else -> showMainPage.postValue(Unit)
             }
         }
