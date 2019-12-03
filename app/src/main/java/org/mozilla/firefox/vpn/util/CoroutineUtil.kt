0d6bec4b002d114d14180f0f864b32dc2c3ce2f7
Roger <lchuang@mozilla.com>
2019-12-03 17:02:44 +0800

    Update algorithm used to determine connected/unstable/no-signal states (#102)
    * Update algo. used to determine connected/unstable/no-signal states
    
    * Separate long line of code into multiple lines

--- app/src/main/java/org/mozilla/firefox/vpn/util/CoroutineUtil.kt
+++ app/src/main/java/org/mozilla/firefox/vpn/util/CoroutineUtil.kt
@@ -2,8 +2,20 @@ package org.mozilla.firefox.vpn.util
 
 import kotlinx.coroutines.CompletionHandler
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 
 fun Job.addCompletionHandler(handle: CompletionHandler): Job {
     this.invokeOnCompletion(handle)
     return this
+}
+
+fun <T, R> Flow<T>.flatMap(transform: suspend (T) -> Flow<R>): Flow<R> {
+    return flow {
+        map(transform).collect { subFlow ->
+            subFlow.collect { emit(it) }
+        }
+    }
 }
