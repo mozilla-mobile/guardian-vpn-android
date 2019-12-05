5e1702707b9c66b2284e4f81e33ffd801a4135ec
Roger <pharspher@gmail.com>
2019-12-05 16:24:07 +0800

    All pages should launch custom tab the same way as login

--- app/src/main/java/org/mozilla/firefox/vpn/main/settings/SettingsFragment.kt
+++ app/src/main/java/org/mozilla/firefox/vpn/main/settings/SettingsFragment.kt
@@ -6,10 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.coroutineScope
 import androidx.navigation.fragment.findNavController
 import coil.api.load
 import coil.transform.CircleCropTransformation
 import kotlinx.android.synthetic.main.fragment_settings.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.mozilla.firefox.vpn.R
 import org.mozilla.firefox.vpn.guardianComponent
 import org.mozilla.firefox.vpn.onboarding.OnboardingActivity
@@ -35,7 +38,9 @@ class SettingsFragment : Fragment() {
             findNavController().navigate(R.id.action_settings_main_to_devices)
         }
         btn_manage_account.setOnClickListener {
-            launchUrl(GuardianService.HOST_FXA)
+            viewLifecycleOwner.lifecycle.coroutineScope.launch(Dispatchers.Main) {
+                launchUrl(GuardianService.HOST_FXA)
+            }
         }
         btn_get_help.setOnClickListener {
             findNavController().navigate(R.id.action_settings_main_to_help)
