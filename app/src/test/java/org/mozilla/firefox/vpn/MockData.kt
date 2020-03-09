bfeb2ba96a9a8adb24e251c4a00de601f15d739e
Kami <yichen@mozilla.com>
2020-03-09 16:01:24 +0800

    Add test for GetServersUseCase

---
+++ app/src/test/java/org/mozilla/firefox/vpn/MockData.kt
@@ -0,0 +1,52 @@
+package org.mozilla.firefox.vpn
+
+import org.mozilla.firefox.vpn.servers.data.CityInfo
+import org.mozilla.firefox.vpn.servers.data.CountryInfo
+import org.mozilla.firefox.vpn.servers.data.ServerInfo
+import org.mozilla.firefox.vpn.service.Server
+
+val testServer = Server(
+    "host1",
+    "ip4",
+    1,
+    true,
+    "public_key",
+    listOf(listOf(25)),
+    "gateway4",
+    "gateway6"
+)
+
+val testUSAServerInfo = ServerInfo(
+    CountryInfo(
+        "USA",
+        "us"
+    ),
+    CityInfo(
+        "Los Angeles, CA",
+        "lax",
+        0.0,
+        0.0
+    ),
+    testServer
+)
+
+val testUSAServerInfo2 = ServerInfo(
+    CountryInfo(
+        "USA",
+        "us"
+    ),
+    CityInfo(
+        "New York, NY",
+        "nyc",
+        0.0,
+        0.0
+    ),
+    testServer
+)
+
+val testServerInfos = listOf(
+    testUSAServerInfo,
+    testUSAServerInfo,
+    testUSAServerInfo2,
+    testUSAServerInfo2
+)
---
+++ app/src/test/java/org/mozilla/firefox/vpn/servers/domain/GetServersUseCaseTest.kt
@@ -0,0 +1,71 @@
+package org.mozilla.firefox.vpn.servers.domain
+
+import com.google.common.truth.Truth.assertThat
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import java.lang.Exception
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.mozilla.firefox.vpn.servers.data.ServerRepository
+import org.mozilla.firefox.vpn.testServerInfos
+import org.mozilla.firefox.vpn.testUSAServerInfo
+import org.mozilla.firefox.vpn.testUSAServerInfo2
+import org.mozilla.firefox.vpn.util.Result
+import org.mozilla.firefox.vpn.util.onError
+import org.mozilla.firefox.vpn.util.onSuccess
+
+class GetServersUseCaseTest {
+
+    @MockK
+    lateinit var serverRepository: ServerRepository
+    lateinit var getServersUseCase: GetServersUseCase
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+        getServersUseCase = GetServersUseCase(serverRepository)
+    }
+
+    @Test
+    fun `When the filter strategy is by country, it should return server list group by country`() {
+        runBlocking {
+            coEvery { serverRepository.getServers() } returns Result.Success(testServerInfos)
+
+            val result = getServersUseCase(FilterStrategy.ByCountry)
+            assertThat(result).isInstanceOf(Result.Success::class.java)
+            result.onSuccess { list ->
+                assertThat(list.size).isEqualTo(1)
+                assertThat(list[0]).isEqualTo(testUSAServerInfo)
+            }
+        }
+    }
+
+    @Test
+    fun `When the filter strategy is by city, it should return server list group by city`() {
+        runBlocking {
+            coEvery { serverRepository.getServers() } returns Result.Success(testServerInfos)
+
+            val result = getServersUseCase(FilterStrategy.ByCity)
+            assertThat(result).isInstanceOf(Result.Success::class.java)
+            result.onSuccess { list ->
+                assertThat(list[0]).isEqualTo(testUSAServerInfo)
+                assertThat(list[1]).isEqualTo(testUSAServerInfo2)
+            }
+        }
+    }
+
+    @Test
+    fun `When getting server from repository is fail, it should also return fail from use case`() {
+        runBlocking {
+            coEvery { serverRepository.getServers() } returns Result.Fail(Exception())
+
+            val result = getServersUseCase(FilterStrategy.ByCountry)
+            assertThat(result).isInstanceOf(Result.Fail::class.java)
+            result.onError { e ->
+                assertThat(e).isInstanceOf(Exception::class.java)
+            }
+        }
+    }
+}
