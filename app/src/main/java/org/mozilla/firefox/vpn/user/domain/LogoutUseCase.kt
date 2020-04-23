b676c74ba356f8255c77fab50387c5f53fe34bcc
Roger Chuang <lchuang@mozilla.com>
2020-04-23 16:30:27 +0800

    Add more log to report

--- app/src/main/java/org/mozilla/firefox/vpn/user/domain/LogoutUseCase.kt
+++ app/src/main/java/org/mozilla/firefox/vpn/user/domain/LogoutUseCase.kt
@@ -2,6 +2,7 @@ package org.mozilla.firefox.vpn.user.domain
 
 import org.mozilla.firefox.vpn.device.data.DeviceRepository
 import org.mozilla.firefox.vpn.user.data.UserRepository
+import org.mozilla.firefox.vpn.util.GLog
 
 class LogoutUseCase(
     private val userRepository: UserRepository,
@@ -9,7 +10,12 @@ class LogoutUseCase(
 ) {
 
     operator fun invoke() {
+        GLog.report(TAG, "logged out")
         userRepository.removeUserInfo()
         deviceRepository.removeDevice()
+    }
+
+    companion object {
+        private const val TAG = "LogoutUseCase"
     }
 }
