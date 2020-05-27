d6c8721d8d9ef3335616c355c6714fe798e530d3
Kami <yichen@mozilla.com>
2020-05-27 12:10:46 +0800

    Use view binding for HelpFragment

--- app/src/main/java/org/mozilla/firefox/vpn/help/HelpFragment.kt
+++ app/src/main/java/org/mozilla/firefox/vpn/help/HelpFragment.kt
@@ -8,36 +8,37 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.coroutineScope
 import androidx.navigation.fragment.findNavController
-import kotlinx.android.synthetic.main.fragment_help.btn_contact_us
-import kotlinx.android.synthetic.main.fragment_help.btn_debug
-import kotlinx.android.synthetic.main.fragment_help.btn_help_and_support
-import kotlinx.android.synthetic.main.fragment_help.toolbar
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import org.mozilla.firefox.vpn.R
 import org.mozilla.firefox.vpn.coreComponent
+import org.mozilla.firefox.vpn.databinding.FragmentHelpBinding
 import org.mozilla.firefox.vpn.guardianComponent
 import org.mozilla.firefox.vpn.report.ReportUtil
 import org.mozilla.firefox.vpn.service.GuardianService
 import org.mozilla.firefox.vpn.util.launchUrl
+import org.mozilla.firefox.vpn.util.viewLifecycle
 import org.mozilla.firefox.vpn.util.viewModel
 
 class HelpFragment : Fragment() {
+
     private val component by lazy {
         HelpComponentImpl(activity!!.coreComponent, activity!!.guardianComponent)
     }
 
     private val viewModel by viewModel { component.viewModel }
 
+    private var binding: FragmentHelpBinding by viewLifecycle()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_help, container, false)
+        binding = FragmentHelpBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel.uiState.observe(viewLifecycleOwner, Observer {
-            btn_debug.visibility = if (it.isDebugEntryVisible) {
+            binding.debugBtn.visibility = if (it.isDebugEntryVisible) {
                 View.VISIBLE
             } else {
                 View.GONE
@@ -55,19 +56,19 @@ class HelpFragment : Fragment() {
             )
         })
 
-        toolbar.setNavigationOnClickListener {
+        binding.toolbar.setNavigationOnClickListener {
             findNavController().popBackStack()
         }
-        btn_contact_us.setOnClickListener {
+        binding.contactUsBtn.setOnClickListener {
             viewLifecycleOwner.lifecycle.coroutineScope.launch(Dispatchers.Main) {
                 launchUrl(GuardianService.HOST_CONTACT)
             }
         }
-        btn_help_and_support.setOnClickListener {
+        binding.helpAndSupportBtn.setOnClickListener {
             viewLifecycleOwner.lifecycle.coroutineScope.launch(Dispatchers.Main) {
                 launchUrl(GuardianService.HOST_SUPPORT)
             }
         }
-        btn_debug.setOnClickListener { viewModel.reportLogClicked() }
+        binding.debugBtn.setOnClickListener { viewModel.reportLogClicked() }
     }
 }
