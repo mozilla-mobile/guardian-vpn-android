0a51a692eb9a9ab20bf7db1ca62dd752ebca0a88
Kami <yichen@mozilla.com>
2020-05-27 11:30:31 +0800

    Add view binding delegate for Fragment

---
+++ app/src/main/java/org/mozilla/firefox/vpn/util/ViewBindingUtil.kt
@@ -0,0 +1,61 @@
+package org.mozilla.firefox.vpn.util
+
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.Observer
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+/*
+* Based on https://gist.github.com/jamiesanson/d1a3ed0910cd605e928572ce245bafc4
+*
+* Refactored to use the preferred `DefaultLifecycleObserver` which does not rely on the annotation processor.
+* See https://developer.android.com/reference/kotlin/androidx/lifecycle/Lifecycle#init
+*
+* Usage:
+* `private var binding: TheViewBinding by viewLifecycle()`
+*/
+
+/**
+ * An extension to bind and unbind a value based on the view lifecycle of a Fragment.
+ * The binding will be unbound in onDestroyView.
+ *
+ * @throws IllegalStateException If the getter is invoked before the binding is set,
+ *                               or after onDestroyView an exception is thrown.
+ */
+fun <T> Fragment.viewLifecycle(): ReadWriteProperty<Fragment, T> =
+    object : ReadWriteProperty<Fragment, T>, DefaultLifecycleObserver {
+
+        private var binding: T? = null
+
+        init {
+            // Observe the view lifecycle of the Fragment.
+            // The view lifecycle owner is null before onCreateView and after onDestroyView.
+            // The observer is automatically removed after the onDestroy event.
+            this@viewLifecycle
+                .viewLifecycleOwnerLiveData
+                .observe(this@viewLifecycle, Observer { owner: LifecycleOwner? ->
+                    owner?.lifecycle?.addObserver(this)
+                })
+        }
+
+        override fun onDestroy(owner: LifecycleOwner) {
+            binding = null
+        }
+
+        override fun getValue(
+            thisRef: Fragment,
+            property: KProperty<*>
+        ): T {
+            return this.binding ?: error("Called before onCreateView or after onDestroyView.")
+        }
+
+        override fun setValue(
+            thisRef: Fragment,
+            property: KProperty<*>,
+            value: T
+        ) {
+            this.binding = value
+        }
+    }
