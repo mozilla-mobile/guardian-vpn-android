d6f558014cb87a5ecabbc4537aa79d491fd31788
Kami <yichen@mozilla.com>
2020-06-16 17:39:08 +0800

    Load installed app icon by Coil

--- app/src/main/java/org/mozilla/firefox/vpn/apptunneling/ui/AppItemViewHolder.kt
+++ app/src/main/java/org/mozilla/firefox/vpn/apptunneling/ui/AppItemViewHolder.kt
@@ -2,6 +2,7 @@ package org.mozilla.firefox.vpn.apptunneling.ui
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import coil.api.load
 import org.mozilla.firefox.vpn.apptunneling.ui.ExpandableItem.AppItem
 import org.mozilla.firefox.vpn.databinding.ItemAppBinding
 
@@ -19,7 +20,7 @@ class AppItemViewHolder(
     fun bind(appItem: AppItem) {
         binding.appName.text = appItem.applicationInfo.loadLabel(binding.root.context.packageManager).toString()
         binding.appPackageName.text = appItem.applicationInfo.packageName
-        binding.appIcon.setImageDrawable(appItem.applicationInfo.loadIcon(binding.root.context.packageManager))
+        binding.appIcon.load("android.resource://${appItem.applicationInfo.packageName}/${appItem.applicationInfo.icon}")
         binding.appCheckbox.isChecked = appItem.type == AppGroupType.PROTECTED
         binding.appCheckbox.isEnabled = appItem.isEnabled
     }
