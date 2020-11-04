4f6790ed5df76016ecb5b8690a82d1449e61ce0a
Grisha Kruglov <gkruglov@mozilla.com>
2020-11-04 14:07:41 -0800

    Revert "Use JetSec to protect sensitive data"
    This reverts commit 9b2e175c731087b560f93637360150f9e0e83a52.

--- app/src/main/java/org/mozilla/firefox/vpn/CoreComponent.kt
+++ app/src/main/java/org/mozilla/firefox/vpn/CoreComponent.kt
@@ -3,13 +3,10 @@ package org.mozilla.firefox.vpn
 import android.app.Application
 import android.content.SharedPreferences
 import androidx.preference.PreferenceManager
-import androidx.security.crypto.EncryptedSharedPreferences
-import androidx.security.crypto.MasterKeys
 
 interface CoreComponent {
     val app: Application
     val prefs: SharedPreferences
-    val encryptedPrefs: SharedPreferences
 }
 
 class CoreComponentImpl(
@@ -18,15 +15,5 @@ class CoreComponentImpl(
 
     override val prefs: SharedPreferences by lazy {
         PreferenceManager.getDefaultSharedPreferences(app)
-    }
-
-    override val encryptedPrefs: SharedPreferences by lazy {
-        EncryptedSharedPreferences.create(
-            app.packageName + "_encrypted_preferences",
-            MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC),
-            app,
-            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
-        )
     }
 }
