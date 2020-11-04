520952e324aaf9e8be6a3c0563e635be659cd302
Grisha Kruglov <gkruglov@mozilla.com>
2020-11-04 14:07:39 -0800

    Revert "Migrate to EncryptedSharedPreferences"
    This reverts commit 214276c74e3af88943eae4faeedb213e8731f6ca.

--- app/src/main/java/org/mozilla/firefox/vpn/user/data/SessionManager.kt
+++ app/src/main/java/org/mozilla/firefox/vpn/user/data/SessionManager.kt
@@ -4,56 +4,33 @@ import android.content.SharedPreferences
 import com.google.gson.Gson
 import org.mozilla.firefox.vpn.user.domain.AuthToken
 
-class SessionManager(
-    private val prefs: SharedPreferences,
-    private val encryptedPrefs: SharedPreferences
-) {
+class SessionManager(private val prefs: SharedPreferences) {
 
     fun saveUserInfo(user: UserInfo) {
         val json = Gson().toJson(user)
-        encryptedPrefs.edit()
+        prefs.edit()
             .putString(PREF_USER_INFO, json)
             .apply()
     }
 
     fun getUserInfo(): UserInfo? {
-        prefs.getString(PREF_USER_INFO, null)?.let {
-            migratePrefs(PREF_USER_INFO, it)
-        }
-
-        return encryptedPrefs.getString(PREF_USER_INFO, null)?.let {
+        return prefs.getString(PREF_USER_INFO, null)?.let {
             Gson().fromJson(it, UserInfo::class.java)
         }
     }
 
     fun saveAuthToken(token: AuthToken) {
-        encryptedPrefs.edit()
+        prefs.edit()
             .putString(PREF_AUTH_TOKEN, token)
             .apply()
     }
 
-    fun getAuthToken(): AuthToken? {
-        prefs.getString(PREF_AUTH_TOKEN, null)?.let {
-            migratePrefs(PREF_AUTH_TOKEN, it)
-        }
-
-        return encryptedPrefs.getString(PREF_AUTH_TOKEN, null)
-    }
+    fun getAuthToken(): AuthToken? = prefs.getString(PREF_AUTH_TOKEN, null)
 
     fun invalidateSession() {
-        encryptedPrefs.edit()
+        prefs.edit()
             .remove(PREF_USER_INFO)
             .remove(PREF_AUTH_TOKEN)
-            .apply()
-    }
-
-    private fun migratePrefs(key: String, value: String) {
-        prefs.edit()
-            .remove(key)
-            .apply()
-
-        encryptedPrefs.edit()
-            .putString(key, value)
             .apply()
     }
 
